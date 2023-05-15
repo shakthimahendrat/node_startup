9fc13953c8b1601ac587d7dadadb881da4df0037
Shakthi Mahendra <58353201+shakthimahendrat@users.noreply.github.com>
2023-05-15 11:42:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World ! this is shathi and abhijeth');
+    res.send('Hello World ! this is shathi');
 
 });
 app.listen(port, () => {
